00cc3ed37223cb9886af04ad0e3d7ab107d435d2
Plidher Luna <65611683+PLNSTD@users.noreply.github.com>
2020-06-15 01:09:24 +0200

    Delete IVolley.kt

--- app/src/main/java/it/uniparthenope/studenti/nappogiuseppe003/lunaplidher001/magicpot/IVolley.kt
+++
@@ -1,8 +0,0 @@
-package it.uniparthenope.studenti.nappogiuseppe003.lunaplidher001.magicpot
-
-import org.json.JSONArray
-import org.json.JSONObject
-
-interface IVolley {
-    fun onResponse(response: JSONArray)
-}
